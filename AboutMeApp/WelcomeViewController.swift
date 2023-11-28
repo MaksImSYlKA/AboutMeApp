29498509b8e8ee2b47d07a44e1cd3651ccb8f124
Maxim Sylka <msylka@me.com>
2023-11-28 21:50:20 +0300

    Create storyboards and configuring controls

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Максим Сылка on 28.11.2023.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    
+    // MARK: - Public Properties
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Initializers
+    
+    // MARK: - Overrides Methods
+    
+    // MARK: - IB Actions
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+}
+
+
+
