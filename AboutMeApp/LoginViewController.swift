c58267f47536c5f360be0073ee279c16319e6194
Maxim Sylka <msylka@me.com>
2023-11-29 21:37:41 +0300

    Create oulets and settings in logInViewController

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    
+    // MARK: - Public Properties
+    
+    // MARK: - Private Properties
+    private let userName = "Maks"
+    private let password = "234"
+    
+    
+    // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - IB Actions
 
-
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+    private func showAlert(withTitle title: String, andMessage message: String){
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        present(alert, animated:true)
+    }
 }
 
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 final class WelcomeViewController: UIViewController {
     
     // MARK: - IB Outlets
+    @IBOutlet var welcomeUserLabel: UILabel!
     
     // MARK: - Public Properties
-    
+    var greeting: String?
+
     // MARK: - Private Properties
     
     // MARK: - Initializers
     
     // MARK: - Overrides Methods
+   
     
     // MARK: - IB Actions
+    @IBAction func cancelButtonAction() {
+        dismiss(animated: true)
+    }
     
     // MARK: - Public Methods
     
